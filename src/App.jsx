bd6b47a44c67d56c462bbb30a61bad7f819cf0af
axlkun <axelcruz.dev@gmail.com>
2024-02-03 20:23:04 -0600

    add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
 
@@ -9,27 +10,64 @@ function App() {
   const MAX = 20000;
   const STEP = 100;
 
-  function handleChange(e){
+  function handleChange(e) {
     setCantidad(+e.target.value);
+  }
+
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('Cantidad invalida');
+      return;
+    }
+
+    setCantidad(valor);
+  }
+
+  function handleClickAumento() {
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert('Cantidad invalida');
+      return;
+    }
+
+    setCantidad(valor);
   }
 
   return (
 
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
 
-     <Header/> 
+      <Header />
 
-     <input 
-     type="range" 
-     className="w-full mt-10 h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
-     onChange={handleChange}
-     min={MIN}
-     max={MAX}
-     step={STEP}
-     value={cantidad}
-     />
+      <div className="flex justify-between my-7">
 
-     <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
+        <Button
+          operador="-"
+          fn={handleClickDecremento}
+        />
+
+        <Button
+          operador="+"
+          fn={handleClickAumento}
+        />
+
+
+      </div>
+
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+      />
+
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
 
     </div>
 
